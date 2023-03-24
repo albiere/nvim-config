1b97c41dc65dd0aae33e82579631d5856ba0d46e
Angelo Albiero Neto <angeloalbiero@hey.com>
2023-03-24 16:59:23 -0300

    Add config for elixirls

--- lua/albiere/plugins/lsp/lspconfig.lua
+++ lua/albiere/plugins/lsp/lspconfig.lua
@@ -67,6 +67,11 @@ lspconfig["solargraph"].setup({
 	on_attach = on_attach,
 })
 
+lspconfig["elixirls"].setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+})
+
 -- configure lua server (with special settings)
 lspconfig["lua_ls"].setup({
 	capabilities = capabilities,
